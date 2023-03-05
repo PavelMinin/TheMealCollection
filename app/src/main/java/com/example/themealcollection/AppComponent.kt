aec25b00470c2401be2f1856627117cb4ef89263
Pavel Minin <minin.pavel@gmail.com>
2023-03-05 17:09:36 +0300

    Working on bottom navigation with multi-module project

--- app/src/main/java/com/example/themealcollection/AppComponent.kt
+++ app/src/main/java/com/example/themealcollection/AppComponent.kt
@@ -6,25 +6,25 @@ import dagger.Component
 import javax.inject.Qualifier
 import javax.inject.Scope
 
-@Component(modules = [AppModule::class])
-interface AppComponent : ArticlesDeps {
-    override val networkService: NetworkService
-
-    @Component.Builder
-    interface Builder {
-
-        @BindsInstance
-        fun application(application: Application): Builder
-
-        @BindsInstance
-        fun apiKey(@NetworkApiQualifier apiKey: String): Builder
-
-        fun build(): AppComponent
-    }
-}
-
-@Qualifier
-annotation class NeApiQualifier
-
-@Scope
-annotation class AppScope
+//@Component(modules = [AppModule::class])
+//interface AppComponent : ArticlesDeps {
+//    override val networkService: NetworkService
+//
+//    @Component.Builder
+//    interface Builder {
+//
+//        @BindsInstance
+//        fun application(application: Application): Builder
+//
+//        @BindsInstance
+//        fun apiKey(@NetworkApiQualifier apiKey: String): Builder
+//
+//        fun build(): AppComponent
+//    }
+//}
+//
+//@Qualifier
+//annotation class NetworkApiQualifier
+//
+//@Scope
+//annotation class AppScope
--- app/src/main/java/com/example/themealcollection/fragments/ContentFragment.kt
+++ app/src/main/java/com/example/themealcollection/fragments/ContentFragment.kt
@@ -1,46 +1,68 @@
 package com.example.themealcollection.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import com.example.core.viewBinding
+import com.example.navigation.TabNavigationFragment
 import com.example.themealcollection.R
 import com.example.themealcollection.databinding.FragmentContentBinding
 
-class ContentFragment : Fragment() {
+class ContentFragment : TabNavigationFragment(R.layout.fragment_content) {
 
-    private var _binding: FragmentContentBinding? = null
-    private val binding get() = requireNotNull(_binding) {
-        "View was destroyed!"
+    private val binding by viewBinding(FragmentContentBinding::bind)
+
+    override val containerId: Int = R.id.content_container
+    override val tabs by lazy {
+        listOf(
+            Tab(getString(R.string.overview_page),
+                com.example.dishes_list.R.navigation.nav_list_flow),
+            Tab(getString(R.string.categories_page),
+                com.example.dishes_list.R.navigation.nav_list_flow),
+            Tab(getString(R.string.areas_page),
+                com.example.dishes_list.R.navigation.nav_list_flow),
+            Tab(getString(R.string.advanced_search_page),
+                com.example.dishes_list.R.navigation.nav_list_flow),
+            Tab(getString(R.string.favorites_page),
+                com.example.dishes_list.R.navigation.nav_list_flow)
+        )
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return FragmentContentBinding.inflate(inflater, container, false)
-            .also { binding ->
-                _binding = binding }
-            .root
-    }
-
+    private lateinit var selectedTab: Tab
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        with(binding) {
-            val nestedController =
-                (childFragmentManager.findFragmentById(R.id.page_container) as NavHostFragment)
-                    .navController
-            bottomNavigation.setupWithNavController(nestedController)
+        selectedTab = tabs[0]
+        selectTab(tabs[0])
+
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.fragment_overview_page -> {
+                    if (selectedTab.name != tabs[0].name) selectTab(tabs[0])
+                    true
+                }
+                R.id.fragment_categories_page -> {
+                    if (selectedTab.name != tabs[1].name) selectTab(tabs[1])
+                    true
+                }
+                R.id.fragment_areas_page -> {
+                    if (selectedTab.name != tabs[2].name) selectTab(tabs[2])
+                    true
+                }
+                R.id.fragment_search_page -> {
+                    if (selectedTab.name != tabs[3].name) selectTab(tabs[3])
+                    true
+                }
+                R.id.fragment_favorites_page -> {
+                    if (selectedTab.name != tabs[4].name) selectTab(tabs[4])
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun tabSelected(tab: Tab) {
+        selectedTab = tab
     }
 }
